223ff83b052ad8d431f45aeaad8fc658139eba16
CalsRanna <calsranna@gmail.com>
2022-03-29 15:28:14 +0800

    feat(database): make some change about database

---
+++ data/sql/custom/db_world/foxycore.sql
@@ -0,0 +1,3 @@
+UPDATE `acore_world`.`item_template` SET `stackable` = 2000 WHERE `stackable` > 1 and `stackable` < 2000;
+
+UPDATE `acore_world`.`item_template` SET `bonding` = 2 WHERE `bonding` = 1;
